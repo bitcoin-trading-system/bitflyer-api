31893fc8a2c7f633e078f1d1727403d12ef4dc42
takuto-oono <takutoono053037@gmail.com>
2024-11-04 02:59:20 +0900

    feat: tomlファイルからconfigを読み込むようにした

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bitcoin-trading-system/bitflyer-api
 go 1.23.2
 
 require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/bitcoin-trading-system/bitflyer-api/config"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	cfg := config.NewConfig()
+
 	router := gin.Default()
 
 	router.GET("/healthcheck", func(c *gin.Context) {
@@ -15,5 +20,8 @@ func main() {
 		})
 	})
 
-	router.Run(":8080")
+	err := router.Run(fmt.Sprintf(":%s", cfg.BaseConfig.Port))
+	if err != nil {
+		log.Fatal("Server Run Failed.: ", err)
+	}
 }
