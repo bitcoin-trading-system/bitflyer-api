7c742653134e9e5b35340ea589602d6fe3c030f6
takuto-oono <takutoono053037@gmail.com>
2024-11-04 02:09:38 +0900

    feat: ginサーバーでhealthcheckのエンドポイントを作成

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
-package bitflyer_api
+package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+
+	router.GET("/healthcheck", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "healthcheck ok!",
+		})
+	})
+
+	router.Run(":8080")
 }
