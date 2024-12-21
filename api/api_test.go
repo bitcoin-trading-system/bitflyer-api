f80af8458725cb4b20be05f8fac624a794d1a90e
takuto-oono <takutoono053037@gmail.com>
2024-12-21 19:05:52 +0900

    feat: publicAPIのテストを作成

--- api/api_test.go
+++ api/api_test.go
@@ -1,1 +1,12 @@
 package api
+
+import (
+	"github.com/bitcoin-trading-system/bitflyer-api/config"
+)
+
+
+var APITestConfig config.Config
+
+func init() {
+	APITestConfig = config.NewConfig("../conf/local.toml")
+}
